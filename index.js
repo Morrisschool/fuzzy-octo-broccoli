9966c7417ff080670fabdd4f546f4aa5a8a8bab5
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-08-28 02:52:12 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+function clear(){
+    for (let i = 1; i <= 7; i++) {
+        let x = document.getElementById('iframemn' + i);
+        x.style.display = "none";
+    }
+}
+
+function mn1(){
+    clear();
+    let x = document.getElementById('iframemn1');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mn2(){
+    clear();
+    let x = document.getElementById('iframemn2');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mn3(){
+    clear();
+    let x = document.getElementById('iframemn3');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mn4(){
+    clear();
+    let x = document.getElementById('iframemn4');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mn5(){
+    clear();
+    let x = document.getElementById('iframemn5');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function mn6(){
+    clear();
+    let x = document.getElementById('iframemn6');
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
