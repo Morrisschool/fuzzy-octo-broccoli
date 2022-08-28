3ff72465ff15a79f62790bfad657571e95aec5ff
Morrisschool <93544921+Morrisschool@users.noreply.github.com>
2022-08-28 03:02:50 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
+install() {
+    printf "\x1B[01;93m========== Updating system ==========\n\x1B[0m"
+    sudo apt update
+    sudo apt upgrade
 
+    printf "\x1B[01;93m========== Install Apache webserver ==========\n\x1B[0m"
+    sudo apt install apache2
+    sudo ufw allow 'Apache'
+
+    printf "\x1B[01;92m================== Done.  ==================\n\x1B[0m\n\n"
+}
+
+# Actually do the install. Put in function and run at end to prevent parcial download and execution.
+install
